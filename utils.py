36063a3ee9e8fe6da2e5a907f72c2d8fc679ba06
johannah <jh1736@gmail.com>
2021-11-21 12:34:10 -0500

    added evaluation code

--- utils.py
+++ utils.py
@@ -11,12 +11,99 @@ import os
 import sys
 import pytz
 from IPython import embed; 
+from opendrift.readers.reader_current_from_drifter import Reader as DrifterReader
+from opendrift.readers.reader_current_from_track import Reader as TrackReader
+from opendrift.readers.reader_netCDF_CF_generic import Reader as GenericReader
+from opendrift.readers.reader_netCDF_CF_unstructured import Reader as UnstructuredReader
+from opendrift.readers.reader_grib2 import Reader as Grib2Reader
+from opendrift.readers.reader_ROMS_native import Reader as ROMSReader
 
 DATA_DIR = '/Volumes/seahorse/2021-drifters/'
 comp_start_time = datetime.datetime(2021, 11, 1, 17, 0, 0, 0, pytz.UTC)
-comp_end_time = comp_start_time + datetime.timedelta(days=10)
+comp_end_time = datetime.datetime(2021, 12, 4, 17, 0, 0, 0, pytz.UTC)
+comp_eval_time = datetime.datetime(2021, 11, 1, 17, 0, 0, 0, pytz.UTC)
 
-def get_weather(start_time=comp_start_time):
+def make_datetimes_from_args(args):
+    start_time =  datetime.datetime(args.start_year, args.start_month, args.start_day, args.start_hour, 0, 0, 0, pytz.UTC)
+    end_time =  start_time + datetime.timedelta(days=args.future_days)
+    start_str = start_time.strftime("%Y%m%d-%H%M")
+    end_str = end_time.strftime("%Y%m%d-%H%M")
+    return start_time, start_str,  end_time, end_str
+ 
+def load_environment(start_time=comp_start_time, download=True, use_gfs=True, use_ncep=False, use_ww3=True, use_rtofs=True):
+    # https://polar.ncep.noaa.gov/global/examples/usingpython.shtml
+    # 'https://tds.hycom.org/thredds/dodsC/GLBy0.08/latest'
+    # //nomads.ncep.noaa.gov/pub/data/nccf/com/rtofs/prod/rtofs.20211120/rtofs
+    if download:
+        download_data()
+        download_predictions(DATA_DIR)
+    remap_gfs = {'u-component_of_wind_planetary_boundary':'x_wind', 
+                 'v-component_of_wind_planetary_boundary':'y_wind'}
+    remap_ww3 = { 'Mean_period_of_wind_waves_surface':'sea_surSignificant_height_of_wind_waves_surface', 
+                  'Significant_height_of_wind_waves_surface':'sea_surface_wave_significant_height',
+                  'u-component_of_wind_surface':'x_wind', 
+                  'v-component_of_wind_surface':'y_wind',
+                  }
+    remap_ncep = {
+                  'reftime1':'reftime', 
+                  'time1':'time', 
+                  'lat':'lat', 
+                  'lon':'lon', 
+                  'u-component_of_wind_surface':'x_wind', 
+                  'v-component_of_wind_surface':'y_wind',
+                  }
+    #
+
+    readers = []
+    # Several models to choose from
+    if use_ncep:
+        # I think ncep is p5 degree (56km)
+        # start: 2021-10-27 06:00:00   end: 2021-11-28 00:00:00   step: 3:00:00
+        ncep_wind_data = GenericReader('/Volumes/seahorse/2021-drifters/ncep/Global_Best_v3.nc', standard_name_mapping=remap_ncep)
+        readers.append(ncep_wind_data)
+    if use_ww3:
+        # https://thredds.ucar.edu/thredds/ncss/grib/NCEP/WW3/Global/Best/dataset.html
+        ww3_wave_data1 = GenericReader('/Volumes/seahorse/2021-drifters/ww3/Global_Best_1101_1111.nc', standard_name_mapping=remap_ww3)
+        # ww3 is only 7 days in advance
+        # 2021-11-10 03:00:00   end: 2021-11-28 00:00:00   step: 3:00:00
+        ww3_wave_data2 = GenericReader('/Volumes/seahorse/2021-drifters/ww3/Global_Best_1110_1203.nc', standard_name_mapping=remap_ww3)
+        readers.append(ww3_wave_data1)
+        readers.append(ww3_wave_data2)
+    if use_rtofs:
+        weather_files = get_rtofs_currents(start_time)
+        fpattern = 'rtofs_glo_2ds_f'
+        last_file = weather_files[-1]
+        for nn in weather_files:
+            try:
+                # readers/basereader/variables.py:                    logger.warning('Assuming time step of 1 hour for ' + self.name)
+                print('load', nn)
+                basename = os.path.split(nn)[1] 
+                # predictions are every hour until 72 hours out
+                hours_delta = 1
+                if last_file == nn:
+                    hours_delta = 10*24 # JUST HOLD THIS LAST FILE FOR A WHILE
+                elif basename.startswith(fpattern):
+                    fcount = int(basename[len(fpattern):len(fpattern)+3])
+                    if fcount >= 72:
+                        hours_delta = 3
+                # modify opendrift to use hours_delta time_step for these files since they are single
+                r = GenericReader(nn, time_step=datetime.timedelta(hours=hours_delta)) 
+                readers.append(r)
+            except:
+                print('could not load', nn)
+
+    if use_gfs:
+        # GFS 0.5 degree (56km)  (higher res than 1 deg)
+        # https://thredds.ucar.edu/thredds/gfsp5 # 14 day
+        # start: 2021-10-21 00:00:00   end: 2021-12-06 12:00:00   step: 3:00:00
+        # gfs should go last in case ww3 is used
+ 
+        gfsp5_wind_data = GenericReader('/Volumes/seahorse/2021-drifters/gfs/Global_0p5deg_Best.nc', standard_name_mapping=remap_gfs)
+        readers.append(gfsp5_wind_data)
+    return readers
+
+
+def get_rtofs_currents(start_time=comp_start_time):
     pred_dir = os.path.join(DATA_DIR, 'pred')
     avail_dates = glob(os.path.join(pred_dir, '2021*'))
     # for days older than today, use nowcast date 
@@ -178,7 +265,7 @@ root group (NETCDF4 data model, file format HDF5):
                         cmd = 'cdo remapnn,global_.08 %s %s'%(target_path, nn_path)
                         os.system(cmd)
 
-def load_data(search_path='data/challenge_*day*.json', start_date=comp_start_time, end_date=comp_end_time):
+def load_drifter_data(search_path='data/challenge_*day*.json', start_date=comp_start_time, end_date=comp_end_time):
     # load sorted dates
     dates = sorted(glob(search_path))
     bad_spots = []
