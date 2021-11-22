b7ab9e45c3565e24a51e570d46f2732bd81d4259
travis <travism@cim.mcgill.ca>
2021-11-22 18:23:31 -0500

    Added error heatmap and scoring csv file

--- evaluate.py
+++ evaluate.py
@@ -26,7 +26,7 @@ from opendrift.readers.reader_current_from_drifter import Reader as DrifterReade
 from opendrift.readers.reader_current_from_track import Reader as TrackReader
 from opendrift.readers.reader_netCDF_CF_generic import Reader as GenericReader
 from opendrift.readers.reader_netCDF_CF_unstructured import Reader as UnstructuredReader
-from opendrift.readers.reader_grib2 import Reader as Grib2Reader
+# from opendrift.readers.reader_grib2 import Reader as Grib2Reader
 from opendrift.readers.reader_ROMS_native import Reader as ROMSReader
 from opendrift.models.openberg import OpenBerg
 from opendrift.models.oceandrift import OceanDrift
@@ -45,7 +45,7 @@ from utils import plot_spot_tracks
 # how to load nomads data: 
 # https://polar.ncep.noaa.gov/global/examples/usingpython.shtml
 
-def evaluate_spot(spot_df, pred_nc):
+def evaluate_spot(spot_df, pred_nc, results_dict):
     spot = np.array(spot_df['spotterId'])[0]
     # create random wind drift factors
     samples = spot_df.index
@@ -133,7 +133,80 @@ def evaluate_spot(spot_df, pred_nc):
     plt.scatter(drifter_lons, drifter_lats, c='b', s=5)
     plt.savefig(os.path.join(load_dir, spot+'_choose.png'))
     plt.close()
-    return np.array(distances)
+
+    # todo: fix for final submission, check heading names, precision and spotter name format, lat format
+    results_dict['spot_id'].append(spot)
+    results_dict['text'].append(spot)
+
+    results_dict['lat_day0'].append(drifter_lats[0])  # 12pm EST
+    results_dict['lon_day0'].append(drifter_lons[0])  # 12pm EST
+    results_dict['pred_lat_day0'].append(pred_lats[0, best_seed])  # 12pm EST
+    results_dict['pred_lon_day0'].append(pred_lons[0, best_seed])  # 12pm EST
+
+    results_dict['lat_day2'].append(drifter_lats[48])  # 12pm EST
+    results_dict['lon_day2'].append(drifter_lons[48])  # 12pm EST
+    results_dict['pred_lat_day2'].append(pred_lats[48, best_seed])  # 12pm EST
+    results_dict['pred_lon_day2'].append(pred_lons[48, best_seed])  # 12pm EST
+    results_dict['err_day2'].append(distances[best_seed][48])  # 12pm EST
+
+    results_dict['lat_day4'].append(drifter_lats[95])  # 11am EST
+    results_dict['lon_day4'].append(drifter_lons[95])  # 11am EST
+    results_dict['pred_lat_day4'].append(pred_lats[95, best_seed])  # 11am EST
+    results_dict['pred_lon_day4'].append(pred_lons[95, best_seed])  # 11am EST
+    results_dict['err_day4'].append(distances[best_seed][95])  # 11am EST (last available)
+
+    day2_err = results_dict['err_day2'][-1]
+    day4_err = results_dict['err_day4'][-1]
+
+    distance_error = np.array([4000, 8000, 16000, 32000, np.iinfo(np.int64).max])
+    day2_score_multiplier_by_distance = [5, 2, 1, 0, 0]
+    day2_score = day2_score_multiplier_by_distance[np.where(day2_err < distance_error)[0][0]]
+    day4_score_multiplier_by_distance = [10, 4, 2, 0, 0]
+    day4_score = day4_score_multiplier_by_distance[np.where(day4_err < distance_error)[0][0]]
+
+    # day6_score_multiplier_by_distance = [25, 10, 6, 1, 0]
+    # day6_score = day6_score_multiplier_by_distance[np.where(day6_err < distance_error)[0][0]]
+    # day8_score_multiplier_by_distance = [50, 20, 10, 2, 0]
+    # day8_score = day8_score_multiplier_by_distance[np.where(day8_err < distance_error)[0][0]]
+    # day10_score_multiplier_by_distance = [100, 40, 20, 5, 0]
+    # day10_score = day10_score_multiplier_by_distance[np.where(day10_err < distance_error)[0][0]]
+
+    score = day2_score + day4_score
+    results_dict['score'].append(score)
+
+    return np.array(distances), results_dict
+
+def create_results_dict():
+    results_dict = {}
+    results_dict['spot_id'] = []
+    results_dict['text'] = []
+    results_dict['lat_day0'] = []
+    results_dict['lon_day0'] = []
+    results_dict['pred_lat_day0'] = []
+    results_dict['pred_lon_day0'] = []
+    results_dict['lat_day2'] = []
+    results_dict['lon_day2'] = []
+    results_dict['pred_lat_day2'] = []
+    results_dict['pred_lon_day2'] = []
+    results_dict['err_day2'] = []
+    results_dict['lat_day4'] = []
+    results_dict['lon_day4'] = []
+    results_dict['pred_lat_day4'] = []
+    results_dict['pred_lon_day4'] = []
+    results_dict['err_day4'] = []
+    results_dict['score'] = []
+    return results_dict
+
+def plot_map(results_dict):
+    import pandas as pd
+    import plotly.express as px
+
+    # df = pd.DataFrame(dict(lat=[24, 22], lon=[60, 92], subreg=[62, 93]))
+    df = pd.DataFrame(results_dict)
+    # fig = px.scatter_geo(df, lat="pred_lat_day2", lon="pred_lon_day2", color="err_day2", text="text", range_color=[0, 32000])
+    fig = px.scatter_geo(df, lat="pred_lat_day4", lon="pred_lon_day4", color="err_day4", text="text", range_color=[0, 32000])
+    fig.update_traces(marker=dict(size=25))
+    fig.show()
 
 if __name__ == '__main__':
     import argparse
@@ -141,29 +214,47 @@ if __name__ == '__main__':
     parser.add_argument('load_dir')
     parser.add_argument('--seed', default=1110)
     parser.add_argument('--eval-type', '-et', default='median', choices=['median'], help='how to choose the lat/lon prediction')
+    parser.add_argument('--plot-only', action='store_true', default=False, help='Show error heatmap over map')
     args = parser.parse_args()
     np.random.seed(args.seed)
     load_dir = args.load_dir
     pred_results = glob(os.path.join(args.load_dir, 'SPOT*.nc'))
     print('found %s predictions'%len(pred_results))
-    if len(pred_results):
+
+    results_dict = create_results_dict()
+
+    if len(pred_results) and not args.plot_only:
         load_args = pickle.load(open(os.path.join(args.load_dir, 'args.pkl'), 'rb'))
         start_time, start_str, end_time, end_str = make_datetimes_from_args(load_args)
         #readers = load_environment(start_time, download=False, use_gfs=load_args.use_gfs, use_ncep=load_args.use_ncep, use_ww3=load_args.use_ww3, use_rtofs=load_args.use_rtofs)
         track_df, wave_df = load_drifter_data(search_path='data/challenge*day*JSON.json', start_date=start_time)
         spot_distances = {}
         running_sum = []
+        i = 0
+        max_spots = 5  # for testing make this small
         for spot_pred_path in pred_results:
-            spot = (os.path.split(spot_pred_path)[1]).split('.')[0]
-            spot_df = track_df[track_df['spotterId'] == spot]
-            spot_nc = nc.Dataset(spot_pred_path)
-            distance = evaluate_spot(spot_df, spot_nc)
-            min_error = distance.sum(1).min()
-            spot_distances[spot] = distance
-            print(spot, min_error)
-            running_sum.append(min_error)
+            if i < max_spots:
+                spot = (os.path.split(spot_pred_path)[1]).split('.')[0]
+                spot_df = track_df[track_df['spotterId'] == spot]
+                spot_nc = nc.Dataset(spot_pred_path)
+                distance, results_dict = evaluate_spot(spot_df, spot_nc, results_dict)
+                min_error = distance.sum(1).min()
+                spot_distances[spot] = distance
+                print(spot, min_error)
+                running_sum.append(min_error)
+                i += 1
 
         print(np.sum(running_sum))
         pickle.dump(spot_distances, open(os.path.join(args.load_dir, 'result_distances.pkl'), 'wb'))
+
+        # todo: fix headings and columns for final submission
+        results_df = pd.DataFrame(dict(spotterId=results_dict['spot_id'],
+                                       pred_lat_day2=results_dict['pred_lat_day2'], pred_lon_day2=results_dict['pred_lon_day2'],
+                                       pred_lat_day4=results_dict['pred_lat_day4'], pred_lon_day4=results_dict['pred_lon_day4'],
+                                       score=results_dict['score']))
+        results_df.to_csv(os.path.join(args.load_dir, 'results_dict.csv'), sep=',', index=False)
+
+    results_dict = pickle.load(open(os.path.join(args.load_dir, 'error_dict.pkl'), 'rb'))
+    plot_map(results_dict)
 
 
